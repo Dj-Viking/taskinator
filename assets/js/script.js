68c7422a56353eaac32b130a233955405a4b6ac5
DJ Viking <anders.swedishviking@gmail.com>
2020-06-23 16:14:53 -0400

    refactored code to have separate functions handling certain things and passing the data from one function into another as an argument for the parameter given into the other

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,40 +9,59 @@ var taskNameInput = document.querySelector("input[name='task-name']");
 //check if its working  
 //console.log(taskNameInput);
 
-function createTaskHandler(){
+function taskFormHandler(event){
     event.preventDefault();
+
     //cool it works. this will store whatever value we type into the text field to be stored into this variable.
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     //checking if its working
     console.dir(taskNameInput);
-    //dynamically creates a new element inside existing HTML code
-    var listItemEl = document.createElement("li");
-    //assigning the CSS class to HTML element we created <li>
-    listItemEl.className = "task-item";//not entering any text here just creating the list item to contain something inside it
-    //listItemEl.textContent = "This is a new task.";
-    //storing the value of the text input area into the newly generated list item element
-    //****dont use this this will put your task name as the task item 'name' lol
-    //****listItemEl.textContent = taskNameInput; 
     //this is attaching the task type that we select from the dropdown menu 
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     console.log(taskTypeInput);//check if its storing correctly
-    //create a <div> container in the HTML to store the task type 
-    var taskInfoEl = document.createElement("div");
-    //class for tasktype
-    taskInfoEl.className = "task-info";
-    //this will set up the dynamic injection of the HTML content into the newly created div to contain the tasktype
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    //inject the innerHTML stored inside the variable taskInfoEl
-    listItemEl.appendChild(taskInfoEl);
-    //finally inject the <div> that contains all of the items that consist of the task-type that we want to inject
-    tasksToDoEl.appendChild(listItemEl);
-    //this will push the new list item Child element that we generated above into the HTML Parent element that we want it in.
-    tasksToDoEl.appendChild(listItemEl);
+
+    //package up data as an object
+    //this is passed into the parameter (event) from the function argument below (taskDataObj)
+    var taskDataObj =
+    {
+        name: taskNameInput,
+        type: taskTypeInput
+    
+    };
+
+    createTaskEl(taskDataObj);
     //ORDER IS IMPORTANT AND SPELLING TOO
-    //console.log(event);
+    console.log(event);
 }
 
-formEl.addEventListener("submit", createTaskHandler);
+
+// function createTaskEl(taskDataObj){
+
+// }
+
+function createTaskEl(taskDataObj){
+    //dynamically creates a new <li> element inside existing HTML code
+    var listItemEl = document.createElement("li");
+    //assigning the CSS class to HTML element we created <li>
+    listItemEl.className = "task-item";//not entering any text here just creating the list item to contain something inside it
+    
+    //create a <div> container in the HTML to store the task type 
+    var taskInfoEl = document.createElement("div");
+    //class for the <div> we created
+    taskInfoEl.className = "task-info";
+
+    //this will set up the dynamic injection of the HTML content into the newly created div to contain the tasktype
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    
+    //inject the innerHTML stored inside the variable taskInfoEl
+    listItemEl.appendChild(taskInfoEl);
+
+    //finally inject the <div> that contains all of the items that consist of the task-type that we want to inject
+    //this will push the new list item Child element that we generated above into the HTML Parent element that we want it in.
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
 
 //buttonEl.addEventListener("click", createTaskHandler);
 //console.log(buttonElement);
