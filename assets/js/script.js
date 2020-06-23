9d984cd7f0d3d57f3bc4c89ad53c6a0376c1e5cb
DJ Viking <anders.swedishviking@gmail.com>
2020-06-23 14:42:52 -0400

    moved svg image file into the right place, created the event to click the button which adds the task-item and the textContent into the submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 //console.dir(window.document);
 
 
-
+var formEl = document.querySelector("#task-form");
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 function createTaskHandler(){
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);//ORDER IS IMPORTANT AND SPELLING TOO
+    //console.log(event);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
-
+//buttonEl.addEventListener("click", createTaskHandler);
 //console.log(buttonElement);
